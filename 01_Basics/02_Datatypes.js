05be28e1d454991a1f91be49f032260b93f08abe
deep-manral <132773335+deep-manral@users.noreply.github.com>
2023-10-26 03:30:53 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,18 @@
+"use strict" ;  //treat all js code as newer version
+// alert(3+3); we are using nodes js not using browser
+
+console.log(3+3);
+console.log("hitesh");
+let name="deepak";
+let age=18;
+let isLoggedIn=false;
+///number-> to power 53
+//bigint
+//boolean->true/false
+//null ->standlone value
+//undefined->
+//symbol ->uniqueness
+
+
+//object
+console.log(typeof "hitesh");
