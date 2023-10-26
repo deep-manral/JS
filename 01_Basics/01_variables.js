e5d46e64ccc1e334b3b8a2bf1ccd4e1a20949941
deep-manral <132773335+deep-manral@users.noreply.github.com>
2023-10-26 03:04:40 +0000

    variables in javascript

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId =14453;
+let accountEmail="deepmanral@.com";
+var accountPassword="12345";
+accountCity="uttrakhand";
+accountEmail="hc@hx.com";
+accountPassword="2121";
+accountCity="benagaluru";
+console.log(accountId);
+// accountId=2;
+/*   
+prefrr not to use var becoause to issue in block scope and funscctional scope */
+console.table([accountId,accountEmail,accountPassword,accountCity]);
