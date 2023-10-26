7a82f03d781d151e83b57f2d42fa841feea68b05
deep-manral <132773335+deep-manral@users.noreply.github.com>
2023-10-26 04:57:32 +0000

    operators

---
+++ 01_Basics/03_conversionoperation.js
@@ -0,0 +1,14 @@
+let score="true";
+console.log(typeof score);
+let valueIntNumber =Number (score);
+console.log( valueIntNumber);
+//"33"->33
+//"33abc"->nan;
+//"true"=>1; false=>0   //basic conversion
+// let isLoggedIN=1;
+// let booleanIsLogedIn=Boolean(isLoggedIN);
+// console.log(booleanIsLogedIn);
+//1=>true  0=>false  //" "=>false    //  "hitesh"=>true
+let someNumber=33;
+let  stringNumber =String(someNumber);
+console.log(typeof stringNumber);
