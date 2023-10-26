0731caf814aefdd8452c9ca6cd03c056b03c55e0
deep-manral <132773335+deep-manral@users.noreply.github.com>
2023-10-26 11:03:18 +0000

    js datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,26 @@
+//Primitive datatype
+// 7 tpes: string,number,bolean,null,undefined,symbol,bigInt,
+const score=100;
+const scoreValue=100.3;
+const isLoggedIn=false;
+const outsideTemp=null;
+let userEmail;
+//symbol
+// const id=Symbol('123');
+// const antherId=Symbol('123');
+// console.log(id===anotherId);
+const bigNumber=343434344456n;
+
+
+//Reference (Non primitive )
+//Array,objects,functions
+const heros= ["shaktiman","naagraj","doga"];
+let myObj={
+    name:"hitesh",
+    age:22,
+}
+const myFunction= function(){
+    console.log("Hello world");
+}
+console.log(typeof bigNumber);
+console.log(typeof heros);
