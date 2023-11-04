5f49c3cbc1cdc89bc633b645600ec9a1b4e9cf99
deep-manral <132773335+deep-manral@users.noreply.github.com>
2023-11-04 13:37:16 +0000

    IIFE

--- 03_basics/this_Arrowfunctions.js
+++ 03_basics/this_Arrowfunctions.js
@@ -1,1 +1,9 @@
 //this shows the current context
+//imefiatly invoked function expression   IIFE
+(function chai(){  //named iife
+    console.log("db connected");
+})();
+(()=>{               //IIFE using arrow function or unnamed iife
+    console.log(`Db connected two ${name}`);
+})('deepak');
+
