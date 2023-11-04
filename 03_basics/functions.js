c83389e5d60e69e00270e732715ae09236479e46
deep-manral <132773335+deep-manral@users.noreply.github.com>
2023-11-04 05:14:10 +0000

    functins and declaration

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -18,10 +18,42 @@ console.log("t");
 // const result=addTwoNumbers(3,4);
 
 // ;
-function loginuserMessage(username){
-    if(username===undefined){
-        console.log("please enter username");
-    }
-    return `${username} just logged in`
+// function loginuserMessage(username){
+//     if(username===undefined){
+//         console.log("please enter username");
+//     }
+//     return `${username} just logged in`
+// }
+// console.log(loginuserMessage("deepak"));
+
+//SHPPPING CART QUERRY
+function calculateCartPrice(val1,val2,...num1){//... is known as rest operater 
+     return num1;
+
 }
-console.log(loginuserMessage("deepak"));
+// console.log(calculateCartPrice(200,377,89));
+const user={
+    username:"hitesh",
+    price:199,
+
+
+}
+function handleObject(anyObject){
+    console.log(`Username is${anyObject.username}and price is ${anyObject.price}`);
+
+
+}
+// handleObject(user);
+//direct object passing
+handleObject({
+    username:"sayMyname",
+    price:300
+
+})
+const myNewArray=[200,400,100,600];
+function returnSecondvalue(getArray){
+return  getArray[2];
+}
+// console.log(returnSecondvalue(myNewArray))
+//directpass Array
+console.log(returnSecondvalue([200,4433,3433,33]));
